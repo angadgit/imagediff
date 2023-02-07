4dcc777caf253aaaef40899fa50cb042cf873061
vedVika teach <vedvika@vedVikas-MacBook-Air.local>
2023-02-07 12:30:57 +0530

    some update

--- src/pages/api/imagediff.js
+++ src/pages/api/imagediff.js
@@ -3,29 +3,37 @@ import { converBase64ToImage } from "convert-base64-to-image";
 import Tesseract from "tesseract.js";
 
 const handler = async (req, res) => {
-//   console.log(req.body.title);
+  //   console.log(req.body.title);
   if (req.method == "POST") {
     if (req.body.img) {
-      const base64 = req.body?.img;
-      const pathToSaveImage = "./image.png";
+      try {
+        const base64 = req.body?.img;
+        const pathToSaveImage = "./image.png";
 
-      converBase64ToImage(base64, pathToSaveImage);
-      const example1 = await Jimp.read(`./Alphabet/${req.body?.title}(1).png`);
-      const example2 = await Jimp.read("./image.png");
-      const check1 = example1.resize(300, Jimp.AUTO);
-      const check2 = example2.resize(300, Jimp.AUTO);
-      const distance = Jimp.distance(example1, example2);
-      const diff = Jimp.diff(check1, check2);
-      const result = diff.percent.toFixed(3) * 1000;
-      const txt = await Tesseract.recognize("./image.png", "eng", {
-        logger: (m) => m,
-      }).then(({ data: { text } }) => {
-        return text;
-      });
-      res.status(200).send({
-        text: txt,
-        result: 100 - result,
-      });
+        converBase64ToImage(base64, pathToSaveImage);
+        const example1 = await Jimp.read(
+          `./Alphabet/${req.body?.title}(1).png`
+        );
+        const example2 = await Jimp.read("./image.png");
+        const check1 = example1.resize(300, Jimp.AUTO);
+        const check2 = example2.resize(300, Jimp.AUTO);
+        const distance = Jimp.distance(example1, example2);
+        const diff = Jimp.diff(check1, check2);
+        const result = diff.percent.toFixed(3) * 1000;
+        const txt = await Tesseract.recognize("./image.png", "eng", {
+          logger: (m) => m,
+        }).then(({ data: { text } }) => {
+          return text;
+        });
+        res.status(200).send({
+          text: txt,
+          result: 100 - result,
+        });
+      } catch (error) {
+        res.status(200).send({
+          message: error.message,
+        });
+      }
     } else {
       res.status(200).send({
         message: "some think wrong",
