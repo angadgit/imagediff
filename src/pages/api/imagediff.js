25f929bdb21bf49203a586e8af5590aae721fe35
vedVika teach <vedvika@vedVikas-MacBook-Air.local>
2023-02-07 11:06:25 +0530

    first commit

--- src/pages/api/imagediff.js
+++ src/pages/api/imagediff.js
@@ -5,32 +5,32 @@ import Tesseract from "tesseract.js";
 const handler = async (req, res) => {
   console.log(req.body);
   if (req.method == "POST") {
-    // if (req.body.img !== null) {
-    const base64 = req.body?.img;
-    const pathToSaveImage = "./image.png";
+    if (req.body.img !== null) {
+      const base64 = req.body?.img;
+      const pathToSaveImage = "./image.png";
 
-    converBase64ToImage(base64, pathToSaveImage);
-    const example1 = await Jimp.read(`./Alphabet/${req.body?.title}(1).png`);
-    const example2 = await Jimp.read("./image.png");
-    const check1 = example1.resize(300, Jimp.AUTO);
-    const check2 = example2.resize(300, Jimp.AUTO);
-    const distance = Jimp.distance(example1, example2);
-    const diff = Jimp.diff(check1, check2);
-    const result = diff.percent.toFixed(3) * 1000;
-    const txt = await Tesseract.recognize("./image.png", "eng", {
-      logger: (m) => m,
-    }).then(({ data: { text } }) => {
-      return text;
-    });
-    res.status(200).send({
-      text: txt,
-      result: 100 - result,
-    });
-    // } else {
-    //   res.status(200).send({
-    //     message: "some think wrong",
-    //   });
-    // }
+      converBase64ToImage(base64, pathToSaveImage);
+      const example1 = await Jimp.read(`./Alphabet/${req.body?.title}(1).png`);
+      const example2 = await Jimp.read("./image.png");
+      const check1 = example1.resize(300, Jimp.AUTO);
+      const check2 = example2.resize(300, Jimp.AUTO);
+      const distance = Jimp.distance(example1, example2);
+      const diff = Jimp.diff(check1, check2);
+      const result = diff.percent.toFixed(3) * 1000;
+      const txt = await Tesseract.recognize("./image.png", "eng", {
+        logger: (m) => m,
+      }).then(({ data: { text } }) => {
+        return text;
+      });
+      res.status(200).send({
+        text: txt,
+        result: 100 - result,
+      });
+    } else {
+      res.status(200).send({
+        message: "some think wrong",
+      });
+    }
   } else if (req.method == "GET") {
     res.status(200).send({
       message: "This is image deff AI APP",
